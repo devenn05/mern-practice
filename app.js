7aeb7af70151aa9525336a733364d730aac8c8b4
devenn05 <rahoolhirlekar.com>
2026-02-17 12:22:02 +0530

    Working on 'fs' with JSON

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs')
 const app = express();
 const port = 3000;
 
@@ -6,42 +7,74 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}))
 
-let myBooks = ["Marvels", "Harry Potter", "DC Universe"]
-let myStories = ["a", "b", "c"]
+const getBooks = ()=>{
+    const jsonData = fs.readFileSync('./books.json', 'utf-8');
+    return JSON.parse(jsonData);
+};
 
-app.get("/about", (req, res)=>{
-    res.render('about')
-})
+const saveBook = (data) =>{
+    fs.writeFileSync('./books.json', JSON.stringify(data, null, 2));
+}
 
 app.get('/', (req, res)=>{
+    const books = getBooks()
     res.render('index', {
-        books: myBooks
+        books: books
     })
 });
 
 app.get('/books/:index', (req, res)=>{
+    const books = getBooks();
     const bookId = req.params.index;
-    const selectedBook = myBooks[bookId];
-    const selectedBookStory = myStories[bookId]
+    const selectedBook = books[bookId];
+
     if (!selectedBook){
         return res.send("Book not found!")
     }
     res.render('book-details', {
-        bookName: selectedBook,
-        bookStory: selectedBookStory,
+        title: selectedBook.title,
+        story: selectedBook.story,
         id: bookId
     })
 })
 
 app.post('/add-book', (req, res) =>{
-    const newBook = req.body.title;
-    const newStory = req.body.story
-    if (newBook && newStory){
-        myBooks.push(newBook);
-        myStories.push(newStory)
+    const books = getBooks();
+    const newBook = {
+        title: req.body.title,
+        story: req.body.story
+    }
+    if (newBook.title && newBook.story){
+        books.push(newBook);
+        saveBook(books)
     }
     res.redirect('/')
 })
+
+app.delete("/delete-book/:index",(req, res)=>{
+    const books = getBooks()
+    const bookId = req.params.index;
+    
+    if (bookId >=0 && bookId < books.length){
+        books.splice(bookId, 1);
+        saveBook(books)
+        res.redirect("/")
+    }else{
+        res.status(404).send("Error, no book found")
+    }
+})
+
+app.post('/delete-all', (req, res)=>{
+    saveBook([]);
+    res.redirect('/')
+})
+
+app.get("/about", (req, res)=>{
+    res.render('about')
+})
+
+
+
 
 app.listen(port, ()=>{
     console.log("Server is running on http://localhost:3000")
