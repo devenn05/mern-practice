7f5734547e8f39304d672b4aa9225ee04bf88ab3
devenn05 <rahoolhirlekar.com>
2026-02-20 16:38:51 +0530

    Added new routes and functionality for updating tasks

--- mongo-practice/controllers/operationController.js
+++ mongo-practice/controllers/operationController.js
@@ -1,12 +1,12 @@
 const Todo = require('../models/todo');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getTasks = catchAsync(async(req, res)=>{
+exports.getTasks = catchAsync(async(req, res, next)=>{
     const todos = await Todo.find({});
     res.render('index', {todos: todos});
 })
 
-exports.addTasks = catchAsync(async(req, res)=>{
+exports.addTasks = catchAsync(async(req, res, next)=>{
     const newTodo = new Todo({
         description: req.body.description,
         priority: req.body.priority
@@ -15,16 +15,32 @@ exports.addTasks = catchAsync(async(req, res)=>{
     res.redirect('/');
 })
 
-exports.deleteTasks = catchAsync(async(req, res)=>{
+exports.deleteTasks = catchAsync(async(req, res, next)=>{
     const { taskId } = req.params;
     await Todo.findByIdAndDelete(taskId);
     res.redirect('/')
 })
 
-exports.updateTaskStatus = catchAsync(async(req, res)=>{
+exports.updateTaskStatus = catchAsync(async(req, res, next)=>{
     const { taskId} = req.params;
     const task = await Todo.findById(taskId);
     const newStatus = task.status === 'pending' ? 'completed' : 'pending';
     await Todo.findByIdAndUpdate(taskId, {status: newStatus});
+    res.redirect('/');
+})
+
+exports.updateButton = catchAsync(async(req, res, next)=>{
+    const {taskId} = req.params;
+    const task = await Todo.findById(taskId)
+    res.render('updateTask', {task: task});
+})
+
+exports.updateWholeTask = catchAsync(async(req, res, next)=>{
+    const {taskId} = req.params;
+    let task = await Todo.findById(taskId);
+    task.description = req.body.description;
+    task.priority = req.body.priority;
+    task.status = req.body.status;
+    await task.save();
     res.redirect('/');
 })
--- mongo-practice/routes/mainRoutes.js
+++ mongo-practice/routes/mainRoutes.js
@@ -6,6 +6,8 @@ router.get('/', operationController.getTasks);
 router.post('/add-task', operationController.addTasks);
 router.post('/delete-task/:taskId', operationController.deleteTasks);
 router.post('/update-status/:taskId', operationController.updateTaskStatus);
+router.get('/update-whole-task/:taskId', operationController.updateButton);
+router.post('/update-whole-task/:taskId', operationController.updateWholeTask);
 
 module.exports = router;
 
