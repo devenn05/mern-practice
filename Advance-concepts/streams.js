4f267cbaa724309223c8b5e8a25e7d1c86ef2c19
devenn05 <rahoolhirlekar.com>
2026-02-23 15:58:30 +0530

    In streams, tried using Transform stream to copy file A to file B while applying filter

--- Advance-concepts/streams.js
+++ Advance-concepts/streams.js
@@ -1,32 +1,63 @@
 // Readable streams
 const fs = require('fs');
-const readStream = fs.createReadStream('./story.txt', {highWaterMark: 5});
-readStream.on('data', (chunk)=>{
-    console.log(chunk.toString());
-});
-readStream.on('end',()=>{
-    console.log('Done!');
-});
+// const readStream = fs.createReadStream('./story.txt', {highWaterMark: 5});
+// readStream.on('data', (chunk)=>{
+//     console.log(chunk.toString());
+// });
+// readStream.on('end',()=>{
+//     console.log('Done!');
+// });
 
-// Writable streams
-const writeStreams = fs.createWriteStream('./output.txt');
-writeStreams.write("Hello, this is chunk One\n");
-writeStreams.write("Hello, this is chunk Two\n");
-writeStreams.end('Final Chunk!!\n');
+// // Writable streams
+// const writeStreams = fs.createWriteStream('./output.txt');
+// writeStreams.write("Hello, this is chunk One\n");
+// writeStreams.write("Hello, this is chunk Two\n");
+// writeStreams.end('Final Chunk!!\n');
 
-writeStreams.on('finish', ()=>{
-    console.log("Done!!!")
+// writeStreams.on('finish', ()=>{
+//     console.log("Done!!!")
+// })
+
+// const writeStreamsExtend = fs.createWriteStream('./output.txt', {flags: 'a'});
+// writeStreamsExtend.write('Welcome back, this is Fourth Chunk. All for of them will be going into the Story!');
+// writeStreamsExtend.on('finish', ()=>{
+//     console.log('Done!!')
+// })
+
+
+// // Combining Read and write (Pipe)  
+// const readStreamsOG = fs.createReadStream('./output.txt');
+// const writeStreamOG = fs.createWriteStream('./story.txt', { flags: 'a' });
+
+// readStreamsOG.pipe(writeStreamOG)
+
+// // A File Transformer Logging System without pipe
+// const readableStream = fs.createReadStream('./input.txt', {highWaterMark: 16});
+// const writableStream = fs.createWriteStream('./backup.txt');
+
+// let chunkCounter = 0;
+
+// readableStream.on('data', (chunks)=>{
+//     writableStream.write(chunks);
+//     chunkCounter ++;
+// })
+
+// readableStream.on('end', ()=>{
+//     console.log("END")
+// })
+
+// Transform Streams
+
+const {Transform} = require('stream');
+
+const reader = fs.createReadStream('./story.txt')
+const writer = fs.createWriteStream('./output.txt')
+
+const uppercaseFilter = new Transform({
+    transform(chunk, encoding, callback){
+        const upperText = chunk.toString().toUpperCase();
+        callback(null, upperText);
+    }
 })
 
-const writeStreamsExtend = fs.createWriteStream('./output.txt', {flags: 'a'});
-writeStreamsExtend.write('Welcome back, this is Fourth Chunk. All for of them will be going into the Story!');
-writeStreamsExtend.on('finish', ()=>{
-    console.log('Done!!')
-})
-
-
-// Combining Read and write (Pipe)  
-const readStreamsOG = fs.createReadStream('./output.txt');
-const writeStreamOG = fs.createWriteStream('./story.txt', { flags: 'a' });
-
-readStreamsOG.pipe(writeStreamOG)
+reader.pipe(uppercaseFilter).pipe(writer);
