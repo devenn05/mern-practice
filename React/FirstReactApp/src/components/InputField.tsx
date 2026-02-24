66fda980f2319e5fdddd648364ca7dd0a768fa2c
devenn05 <rahoolhirlekar.com>
2026-02-24 18:26:50 +0530

    Refactor InputField component to handle multiple form fields and store data in state by create a Interface and using it to collect and display Data.

--- React/FirstReactApp/src/components/InputField.tsx
+++ React/FirstReactApp/src/components/InputField.tsx
@@ -1,26 +1,70 @@
-import React, { useState, type ChangeEvent } from 'react'
+import React, { useState, type ChangeEvent, type FormEvent } from 'react'
+
+interface Person{
+    name: string,
+    age: string,
+    gender: string
+}
 
 const InputField = () => {
-    const [name, setName] = useState<string>('');
-    const [nameList, setNameList] = useState<string[]>([]);
-    const handleEntryChange = (e: ChangeEvent<HTMLInputElement>): void =>{
-        setName(e.target.value);
+    const [formData, setFormData] = useState<Person>({
+        name: '',
+        age: '',
+        gender: 'Other'
+    });
+    const [people, setPeople] = useState<Person[]>([]);
+    
+    const updateName = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        const newValue = e.target.value;
+        setFormData({...formData, name: newValue});
     }
-    const handleSubmit = (e: ChangeEvent<HTMLFormElement>): void =>{
-        e.preventDefault()
-        if (name.trim()){
-            setNameList([...nameList, name.trim()]);
-            console.log(`${name} is added.`);
-            setName('');
-        }
+    const updateAge = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        const newValue = e.target.value;
+        setFormData({...formData, age: newValue});
     }
+    const updateGender = (e: React.ChangeEvent<HTMLSelectElement>) =>{
+        const newValue = e.target.value;
+        setFormData({...formData, gender: newValue});
+    }
+    const handleSubmit = (e: FormEvent)=>{
+        e.preventDefault();
+        setPeople([...people, formData]);
+        setFormData({name: '', age: '', gender: 'Other'})
+    }
+
   return (
     <div>
     <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={handleEntryChange} placeholder='Enter your name' />
-        <button type='submit'>Send</button>
+        <input type="text" value={formData.name} onChange={updateName} placeholder='Enter your Name' />
+        <input type="number" value={formData.age} onChange={updateAge} placeholder='Enter your Age' />
+        <select value={formData.gender} onChange={updateGender}>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+        </select>
+        <button type='submit'>Add Person</button>
     </form>
-    <p>Length: {nameList.length}</p>
+    <table>
+        <thead>
+            <tr>
+                <th>Index</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            {people.map((item, index)=>(
+                <tr key = {index}>
+                    <th>{index + 1}</th>
+                    <th>{item.name}</th>
+                    <th>{item.age}</th>
+                    <th>{item.gender}</th>
+                </tr>
+            ))}
+        </tbody>
+    </table>
     </div>
   )
 }
