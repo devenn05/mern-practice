cd66ac2120226b37adaf006219b75dc497fd1638
devenn05 <rahoolhirlekar.com>
2026-03-02 15:30:05 +0530

    Added useEffect hook to persist tasks in local storage.

--- React/To Do Manager/src/App.tsx
+++ React/To Do Manager/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Taskbar from './components/Taskbar'
 import TaskTable from './components/TaskTable'
@@ -13,7 +13,18 @@ export interface Task{
 
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([])
+  const [tasks, setTasks] = useState<Task[]>(()=>{
+    const saved = localStorage.getItem('my_tasks')
+    if (saved){
+      return JSON.parse(saved)
+    }
+    return [];
+  })
+
+  useEffect(()=>{
+    localStorage.setItem('my_tasks', JSON.stringify(tasks));
+  }, [tasks])
+
   return (
     <>
       <Taskbar tasks={tasks} setTasks={setTasks} />
